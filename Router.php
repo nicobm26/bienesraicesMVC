2ad160f9cf4f978713ec9f38d3341afca2178973
nicolasb <nicolasbotello@hotmail.com>
2023-10-17 10:20:52 -0500

    creando el metodo para mostrar vista en la clase router

--- Router.php
+++ Router.php
@@ -7,7 +7,7 @@ class Router {
     public $rutasGet=[];
     public $rutasPost=[];
 
-    public function get($url, $funcion){
+    public function get( string $url, array $funcion){
         $this->rutasGet[$url] = $funcion;
         // echo "la url es " . $url;echo '<br>';
         // echo '<pre>';var_dump($funcion);echo '</pre>';
@@ -25,12 +25,20 @@ class Router {
 
         if($funcion){
             // La url existe y hay una funcion asociada
-            // debugear($funcion);
-            echo '<pre>';var_dump($funcion);echo '</pre>';
-            echo '<pre>';var_dump($this);echo '</pre>';echo '<br>';
+
+            // echo "funcion: "; echo '<pre>';var_dump($funcion);echo '</pre>';
+            // echo "EL objeto this"; echo '<pre>';var_dump($this);echo '</pre>';echo '<br>';
             call_user_func($funcion, $this);
+            // call_user_func(callable, parametros opcionales)
+            //callable -> Puede ser el nombre de la función como una cadena, un array con un objeto y un nombre de método, o una función anónima.
+            // Parámetros opcionales que se pueden pasar a la función que se va a llamar.
         }else{
             debugear("error 404");
         }
+    }
+
+    // Muestra una vista
+    public function mostrarVista($view) {
+        include __DIR__ . "/views/$view.php";
     }
 }
