8740f0822e45ac2c516eb66699263272b1472c5f
nicolasb <nicolasbotello@hotmail.com>
2023-10-18 11:52:52 -0500

    modifico el metodo crear del controller propiedad

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -4,16 +4,22 @@ namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
+// use Intervention\Image as Image;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PropiedadController{
 
     public static function index(Router $router){
-    // debugear($router);
+
     $propiedades = Propiedad::all();
-    $resultado = null;
+    $vendedores = Vendedor::all();
+
+    $resultado = $_GET["resultado"] ?? null;
+
     $router->mostrarVista("propiedades/admin", [ 
-        "propiedades" => $propiedades,
-        "resultado" => $resultado
+            'propiedades' => $propiedades,
+            'vendedores' => $vendedores,
+            'resultado' => $resultado
         ]);
     }
 
@@ -21,14 +27,49 @@ class PropiedadController{
 
         $propiedad = new Propiedad();
         $vendedores = Vendedor::all();
+        $errores = Propiedad::getErrores();
+        
 
-        if($_SERVER['REQUEST_METHOD'] === "POST"){
-            debugear($_POST);
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  
+            $propiedad = new Propiedad($_POST['propiedad']);
+            
+            // debugear($_FILES);
+            $image = null;
+            //Genera un nombre unico
+            $nombreImagen = md5( uniqid( rand(), true) ) . ".jpg";
+
+            if($_FILES['propiedad']['tmp_name']['imagen']){
+                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
+                $propiedad->setImagen($nombreImagen);
+            }
+    
+            // debugear($_SERVER["DOCUMENT_ROOT"]);
+            
+            // Validar
+            $errores = $propiedad->validar();
+            if(empty($errores)){
+        
+                // Crear la carpeta para subir imagenes
+                if(!is_dir(CARPETA_IMAGENES)){
+                    mkdir(CARPETA_IMAGENES);
+                }    
+                //Guarda la imagen en el servidor
+                $image->save(CARPETA_IMAGENES . $nombreImagen);
+                
+                // Guarda en la base de datos
+                $resultado = $propiedad->guardar();
+
+                if($resultado) {
+                    header('location: /propiedades');
+                }
+            }
         }
         
         $router->mostrarVista('propiedades/crear',[
             "propiedad" => $propiedad,
-            "vendedores" => $vendedores
+            "vendedores" => $vendedores,
+            "errores" => $errores
         ]);
     }
 
