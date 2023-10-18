bb9d1df669329f5ccca990269414668bc58a05a2
nicolasb <nicolasbotello@hotmail.com>
2023-10-18 09:47:21 -0500

    trabajar con peticiones post en el controlador

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
+use Model\Vendedor;
 
 class PropiedadController{
 
@@ -13,12 +14,22 @@ class PropiedadController{
     $router->mostrarVista("propiedades/admin", [ 
         "propiedades" => $propiedades,
         "resultado" => $resultado
-    ]);
-
+        ]);
     }
 
-    public static function crear(){
-        echo "crear propiedad";
+    public static function crear(Router $router){
+
+        $propiedad = new Propiedad();
+        $vendedores = Vendedor::all();
+
+        if($_SERVER['REQUEST_METHOD'] === "POST"){
+            debugear($_POST);
+        }
+        
+        $router->mostrarVista('propiedades/crear',[
+            "propiedad" => $propiedad,
+            "vendedores" => $vendedores
+        ]);
     }
 
     public static function actualizar(){
