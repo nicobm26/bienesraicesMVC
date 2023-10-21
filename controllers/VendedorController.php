9304a14e817bc2868a1f874fa55598df47978d04
nicolasb <nicolasbotello@hotmail.com>
2023-10-21 15:56:12 -0500

    logica del metodo actualiar del vendedor controller

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -29,8 +29,31 @@ class VendedorController {
         ]);
     }
 
-    public static function actualizar()  {
-    echo "actualizar";
+    public static function actualizar(Router $router)  {
+        
+        $id = $_GET["id"];
+        // $id = validarORedireccionar("/admin");
+        $errores = Vendedor::getErrores();
+        $vendedor = Vendedor::findById($id);
+
+        if($_SERVER['REQUEST_METHOD']=== 'POST'){
+            
+            $args = $_POST['vendedor'];
+            // debugear($args);
+
+            $vendedor->sincronizar($args);
+
+            $errores = $vendedor->validar();
+            
+            if(empty($errores)){
+                $vendedor->guardar();
+            }
+        }
+
+        $router->mostrarVista("vendedores/actualizar",[
+            "errores" => $errores,
+            "vendedor" => $vendedor
+        ]);
     }
 
     public static function eliminar()  {
