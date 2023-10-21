9d9f134b92dc93c45c54ad9d1dd61e210a0698cf
nicolasb <nicolasbotello@hotmail.com>
2023-10-21 15:02:13 -0500

    modificacion en la vista admin

--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -10,7 +10,7 @@
     } ?>
     
     <a href="/propiedades/crear" class="boton-verde-inlineBlock">Nueva Propiedad</a>
-    <a href="/admin/vendedores/crear.php" class="boton-amarillo">Nuevo Vendedor</a>
+    <a href="/vendedor/crear" class="boton-amarillo">Nuevo Vendedor</a>
     <h2>Propiedades</h2>
     <table class="propieades">
         <thead>
@@ -40,6 +40,35 @@
                 </tr>
             <?php endforeach ?>
         </tbody>
-
     </table>
+
+    <h2>Vendedores</h2>
+        <table class="propieades">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Telefono</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>  <!-- 4. Mostrar los resultado-->
+            <?php foreach ($vendedores as $vendedor): ?>
+                <tr>
+                    <td> <?php echo $vendedor->id; ?> </td>
+                    <td> <?php echo $vendedor->nombre . " " . $vendedor->apellido; ?></td>
+                    <td> <?php echo $vendedor->telefono; ?></td>
+                    <td>
+                        <form method="post" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $vendedor->id ?>">
+                            <input type="hidden" name="tipo" value="vendedor">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a href="/vendedor/actualizar?id=<?php echo $vendedor->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+
 </main>
