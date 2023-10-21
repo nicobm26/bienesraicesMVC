d1c8a47e423c34aba1122685ad24c24d500b1b90
nicolasb <nicolasbotello@hotmail.com>
2023-10-21 15:01:03 -0500

    registro las interfaces de vendedor en el index

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . "/../includes/app.php";
 
 use MVC\Router;
 use Controllers\PropiedadController;
+use Controllers\VendedorController;
 
 $router = new Router();
 
@@ -15,6 +16,14 @@ $router->post("/propiedades/crear", [PropiedadController::class, 'crear']);
 $router->get("/propiedades/actualizar", [PropiedadController::class, 'actualizar']);
 $router->post("/propiedades/actualizar", [PropiedadController::class, 'actualizar']);
 $router->post("/propiedades/eliminar", [PropiedadController::class, 'eliminar']);
+
+
+$router->get("/vendedor/crear", [VendedorController::class , 'crear']);
+$router->post("/vendedor/crear", [VendedorController::class , 'crear']);
+$router->get("/vendedor/actualizar", [VendedorController::class , 'actualizar']);
+$router->post("/vendedor/actualizar", [VendedorController::class , 'actualizar']);
+$router->post("/vendedor/eliminar", [VendedorController::class , 'eliminar']);
+
 
 $router->comprobarRutas();
 
